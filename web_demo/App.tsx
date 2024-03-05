33b9e73de009a3f0e90380916fc7e1fbe78e279a
DongOk <hcan159@naver.com>
2024-03-05 17:29:16 +0900

    @re-setup

--- web_demo/App.tsx
+++ web_demo/App.tsx
@@ -1,35 +1,10 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Pressable,
-  useWindowDimensions,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  const {height} = useWindowDimensions();
-  const [getnumber, setNumber] = useState<number>(0);
-
-  function handlePress() {
-    let dummyNumber = parseInt(`${Math.random() * 10000}`, 10);
-    setNumber(dummyNumber % 100);
-  }
-
   return (
-    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
-      <Text>Random number: {getnumber}</Text>
-      <View style={styles.br} />
-      <Pressable
-        style={({pressed}) => [
-          {
-            opacity: pressed ? 0.7 : 1,
-          },
-          styles.btn,
-        ]}
-        onPress={handlePress}>
-        <Text style={styles.btnText}>Generate a number</Text>
-      </Pressable>
+    <View style={[styles.container, StyleSheet.absoluteFill]}>
+      <Text>Hello</Text>
     </View>
   );
 }
