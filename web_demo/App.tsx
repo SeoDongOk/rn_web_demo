2d8d85b434856d635ba824efd6a81046ed61c589
DongOk <hcan159@naver.com>
2024-03-06 20:35:52 +0900

    @add rn-navigation

--- web_demo/App.tsx
+++ web_demo/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import MyTabs from './app/home/Home';
 
 export default function App() {
   return (
     <View style={[styles.container, StyleSheet.absoluteFill]}>
       <Text>Hello</Text>
+      <MyTabs />
     </View>
   );
 }
--- web_demo/app/home/Home.tsx
+++ web_demo/app/home/Home.tsx
@@ -1,5 +1,44 @@
-import {Platform, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {Dummy} from '../components/dummy';
+import {NavigationContainer} from '@react-navigation/native';
+// import MaterialCommunityIcons from 'react-native-vector-icons';
 
-const styles = StyleSheet.create({
-  height: Platform.OS === 'web' ? 200 : 100,
-});
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={{
+          tabBarActiveTintColor: '#e91e63',
+        }}>
+        <Tab.Screen
+          name="Feed"
+          component={Dummy}
+          options={{
+            tabBarLabel: 'Home',
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={Dummy}
+          options={{
+            tabBarLabel: 'Updates',
+            tabBarBadge: 3,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Dummy}
+          options={{
+            tabBarLabel: 'Profile',
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MyTabs;
